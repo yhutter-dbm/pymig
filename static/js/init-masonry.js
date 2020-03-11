e93d5b36bd383db7f9856afcfe1663ac4b9233f3
Yannick Hutter <yannick.hutter@solve.ch>
2020-03-11 16:06:14 +0100

    Replaced Skeleton with UIKit and used masonry for auto image layout

---
+++ static/js/init-masonry.js
@@ -0,0 +1,13 @@
+// Took inspiration from this: https://codepen.io/desandro/pen/MwJoZQ
+var grid = document.querySelector('.grid');
+
+var msnry = new Masonry( grid, {
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+});
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
